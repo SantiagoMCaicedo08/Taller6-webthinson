6fa41decc528edeb9da58e05ee793bf6b3738d25
SantiagoMCaicedo08 <104151933+SantiagoMCaicedo08@users.noreply.github.com>
2024-07-06 02:02:14 -0500

    Delete crud_project directory

--- crud_project/main_app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-class MainAppConfig(AppConfig):
-    name = 'main_app'
-
-    def ready(self):
-        from . import signals  # Importar las señales para que se registren
